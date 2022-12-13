fc32c8cccdc7088fdfca6ef7850b4eb6e1b35c33
sudipnext <parajulisudip123@outlook.com>
2022-12-13 17:57:51 +0545

    updated!

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 
-window.addEventListener(
-  "hashchange",
-  () => {
-    document.querySelector(".activity").classList.remove("activity");
-    document
-      .querySelector('[href="' + window.location.hash + '"]')
-      .classList.add("activity");
-  },
-  false
-);
 navshow = 0;
 
 function navbar() {
   var element = document.body;
   element.classList.toggle("show");
 }
+
+// got top
