265d69bc8583d12475b906288ea37b2ab47cf4a4
sudipnext <parajulisudip123@outlook.com>
2023-07-14 09:42:17 +0545

    updated

--- EXCESSSITE/data/certpy.js
+++ EXCESSSITE/data/certpy.js
@@ -1,9 +1,206 @@
-let data = [{ "cert_id": "#079-PDJ-01", "Name": "Bijaya Giri", "type": "Participation + Completion", "completion": "9th July", "percentage": "90%" }, { "cert_id": "#079-PDJ-02", "Name": "Raghav Raj Mudvari", "type": "Only Participated", "completion": "9th July", "percentage": "70%" }, { "cert_id": "#079-PDJ-03", "Name": "Yubshan Shrestha", "type": "Participation + Completion", "completion": "9th July", "percentage": "90%" }, { "cert_id": "#079-PDJ-04", "Name": "Suraj Acharya", "type": "Only Participated", "completion": "9th July", "percentage": "70%" }, { "cert_id": "#079-PDJ-05", "Name": "Roshan Chaudhary", "type": "Only Participated", "completion": "9th July", "percentage": "70%" }, { "cert_id": "#079-PDJ-06", "Name": "Sonu Kumar Gupta", "type": "Participation + Completion", "completion": "9th July", "percentage": "90%" }, { "cert_id": "#079-PDJ-07", "Name": "Rajesh Pandey", "type": "Participation + Completion", "completion": "9th July", "percentage": "90%" }, { "cert_id": "#079-PDJ-08", "Name": "Nigam Yadav", "type": "Participation + Completion", "completion": "9th July", "percentage": "90%" }, { "cert_id": "#079-PDJ-09", "Name": "Yogendra Karna", "type": "Only Participated", "completion": "9th July", "percentage": "70%" }, { "cert_id": "#079-PDJ-10", "Name": "Tilak Thapa", "type": "Only Participated", "completion": "9th July", "percentage": "70%" }, { "cert_id": "#079-PDJ-11", "Name": "Rahul Karna", "type": "Participation + Completion", "completion": "9th July", "percentage": "90%" }, { "cert_id": "#079-PDJ-12", "Name": "Sumit Rijal", "type": "Participation + Completion", "completion": "9th July", "percentage": "90%" }, { "cert_id": "#079-PDJ-13", "Name": "Niruta Niraula", "type": "Participation + Completion", "completion": "9th July", "percentage": "90%" }, { "cert_id": "#079-PDJ-14", "Name": "Anuja Neupane", "type": "Participation + Completion", "completion": "9th July", "percentage": "90%" }, { "cert_id": "#079-PDJ-15", "Name": "Anusha Shrestha", "type": "Participation + Completion", "completion": "9th July", "percentage": "90%" }, { "cert_id": "#079-PDJ-16", "Name": "Arun Shah", "type": "Only Participated", "completion": "9th July", "percentage": "70%" }, { "cert_id": "#079-PDJ-17", "Name": "Soniya Rajbanshi", "type": "Participation + Completion", "completion": "9th July", "percentage": "90%" }, { "cert_id": "#079-PDJ-18", "Name": "Roshan Kumar Kanu", "type": "Only Participated", "completion": "9th July", "percentage": "70%" }, { "cert_id": "#079-PDJ-19", "Name": "Prachit Kumar Bhujel", "type": "Only Participated", "completion": "9th July", "percentage": "70%" }, { "cert_id": "#079-PDJ-20", "Name": "Sandesh Poudel", "type": "Only Participated", "completion": "9th July", "percentage": "70%" }, { "cert_id": "#079-PDJ-21", "Name": "Binita gautam", "type": "Not Present", "completion": "9th July", "percentage": "90%" }, { "cert_id": "#079-PDJ-22", "Name": "Utsav Tamrakar", "type": "Only Participated", "completion": "9th July", "percentage": "70%" }, { "cert_id": "#079-PDJ-23", "Name": "Yogesh Bhandari ", "type": "Only Participated", "completion": "9th July", "percentage": "70%" }, { "cert_id": "#079-PDJ-24", "Name": "Sony Kumari Chaudhary ", "type": "Participation + Completion", "completion": "9th July", "percentage": "90%" }, { "cert_id": "#079-PDJ-25", "Name": "Aayush Gautam", "type": "Participation + Completion", "completion": "9th July", "percentage": "90%" }, { "cert_id": "#079-PDJ-26", "Name": "Sudip Phuyal ", "type": "Only Participated", "completion": "9th July", "percentage": "70%" }, { "cert_id": "#079-PDJ-27", "Name": "Manjil dahal", "type": "Only Participated", "completion": "9th July", "percentage": "70%" }, { "cert_id": "#079-PDJ-28", "Name": "Bikash Kumar Das ", "type": "Only Participated", "completion": "9th July", "percentage": "70%" }, { "cert_id": "#079-PDJ-29", "Name": "Suwarna Pyakurel", "type": "Only Participated", "completion": "9th July", "percentage": "70%" }]
-
+let data = [
+  {
+    cert_id: "#079-PDJ-01",
+    Name: "Bijaya Giri",
+    type: "Accomplished Participant",
+    completion: "9th July",
+    percentage: "A+",
+  },
+  {
+    cert_id: "#079-PDJ-02",
+    Name: "Raghav Raj Mudvari",
+    type: "Engaged Participant",
+    completion: "9th July",
+    percentage: "A",
+  },
+  {
+    cert_id: "#079-PDJ-03",
+    Name: "Yubshan Shrestha",
+    type: "Accomplished Participant",
+    completion: "9th July",
+    percentage: "A+",
+  },
+  {
+    cert_id: "#079-PDJ-04",
+    Name: "Suraj Acharya",
+    type: "Engaged Participant",
+    completion: "9th July",
+    percentage: "A",
+  },
+  {
+    cert_id: "#079-PDJ-05",
+    Name: "Roshan Chaudhary",
+    type: "Engaged Participant",
+    completion: "9th July",
+    percentage: "A",
+  },
+  {
+    cert_id: "#079-PDJ-06",
+    Name: "Sonu Kumar Gupta",
+    type: "Accomplished Participant",
+    completion: "9th July",
+    percentage: "A+",
+  },
+  {
+    cert_id: "#079-PDJ-07",
+    Name: "Rajesh Pandey",
+    type: "Accomplished Participant",
+    completion: "9th July",
+    percentage: "A+",
+  },
+  {
+    cert_id: "#079-PDJ-08",
+    Name: "Nigam Yadav",
+    type: "Accomplished Participant",
+    completion: "9th July",
+    percentage: "A+",
+  },
+  {
+    cert_id: "#079-PDJ-09",
+    Name: "Yogendra Karna",
+    type: "Engaged Participant",
+    completion: "9th July",
+    percentage: "A",
+  },
+  {
+    cert_id: "#079-PDJ-10",
+    Name: "Tilak Thapa",
+    type: "Engaged Participant",
+    completion: "9th July",
+    percentage: "A",
+  },
+  {
+    cert_id: "#079-PDJ-11",
+    Name: "Rahul Karna",
+    type: "Accomplished Participant",
+    completion: "9th July",
+    percentage: "A+",
+  },
+  {
+    cert_id: "#079-PDJ-12",
+    Name: "Sumit Rijal",
+    type: "Accomplished Participant",
+    completion: "9th July",
+    percentage: "A+",
+  },
+  {
+    cert_id: "#079-PDJ-13",
+    Name: "Niruta Niraula",
+    type: "Accomplished Participant",
+    completion: "9th July",
+    percentage: "A+",
+  },
+  {
+    cert_id: "#079-PDJ-14",
+    Name: "Anuja Neupane",
+    type: "Accomplished Participant",
+    completion: "9th July",
+    percentage: "A+",
+  },
+  {
+    cert_id: "#079-PDJ-15",
+    Name: "Anusha Shrestha",
+    type: "Accomplished Participant",
+    completion: "9th July",
+    percentage: "A+",
+  },
+  {
+    cert_id: "#079-PDJ-16",
+    Name: "Arun Shah",
+    type: "Engaged Participant",
+    completion: "9th July",
+    percentage: "A",
+  },
+  {
+    cert_id: "#079-PDJ-17",
+    Name: "Soniya Rajbanshi",
+    type: "Accomplished Participant",
+    completion: "9th July",
+    percentage: "A+",
+  },
+  {
+    cert_id: "#079-PDJ-18",
+    Name: "Roshan Kumar Kanu",
+    type: "Engaged Participant",
+    completion: "9th July",
+    percentage: "A",
+  },
+  {
+    cert_id: "#079-PDJ-19",
+    Name: "Prachit Kumar Bhujel",
+    type: "Engaged Participant",
+    completion: "9th July",
+    percentage: "A",
+  },
+  {
+    cert_id: "#079-PDJ-20",
+    Name: "Sandesh Poudel",
+    type: "Engaged Participant",
+    completion: "9th July",
+    percentage: "A",
+  },
+  {
+    cert_id: "#079-PDJ-22",
+    Name: "Utsav Tamrakar",
+    type: "Engaged Participant",
+    completion: "9th July",
+    percentage: "A",
+  },
+  {
+    cert_id: "#079-PDJ-23",
+    Name: "Yogesh Bhandari ",
+    type: "Engaged Participant",
+    completion: "9th July",
+    percentage: "A",
+  },
+  {
+    cert_id: "#079-PDJ-24",
+    Name: "Sony Kumari Chaudhary ",
+    type: "Accomplished Participant",
+    completion: "9th July",
+    percentage: "A+",
+  },
+  {
+    cert_id: "#079-PDJ-25",
+    Name: "Aayush Gautam",
+    type: "Accomplished Participant",
+    completion: "9th July",
+    percentage: "A+",
+  },
+  {
+    cert_id: "#079-PDJ-26",
+    Name: "Sudip Phuyal ",
+    type: "Engaged Participant",
+    completion: "9th July",
+    percentage: "A",
+  },
+  {
+    cert_id: "#079-PDJ-27",
+    Name: "Manjil dahal",
+    type: "Engaged Participant",
+    completion: "9th July",
+    percentage: "A",
+  },
+  {
+    cert_id: "#079-PDJ-28",
+    Name: "Bikash Kumar Das ",
+    type: "Engaged Participant",
+    completion: "9th July",
+    percentage: "A",
+  },
+  {
+    cert_id: "#079-PDJ-29",
+    Name: "Suwarna Pyakurel",
+    type: "Engaged Participant",
+    completion: "9th July",
+    percentage: "A",
+  },
+];
 
 const input = document.getElementById("lname");
 const msg = document.querySelector(".alert");
 const result = document.querySelector(".message");
+const suggestionList = document.getElementById("suggestionList");
 
 input.addEventListener("input", () => {
   msg.classList.add("hide");
@@ -28,16 +225,55 @@ input.addEventListener("input", () => {
   return isValid;
 });
 
+input.addEventListener("input", () => {
+  const searchValue = input.value.trim().toLowerCase();
+  let suggestions = [];
+
+  if (searchValue) {
+    suggestions = data.filter((item) => {
+      const certId = item.cert_id.toLowerCase();
+    
+
+      // return certId.includes(searchValue) || name.includes(searchValue);
+      return certId.includes(searchValue)
+    });
+  }
+
+  displaySuggestions(suggestions);
+});
+
+function displaySuggestions(suggestions) {
+  suggestionList.innerHTML = "";
+
+  if (suggestions.length === 0) {
+    suggestionList.classList.add("hide");
+    return;
+  }
+
+  suggestionList.classList.remove("hide");
+
+  suggestions.forEach((item) => {
+    const suggestionItem = document.createElement("div");
+    suggestionItem.classList.add("suggestion-item");
+    suggestionItem.textContent = `${item.cert_id}`;
+
+    suggestionItem.addEventListener("click", () => {
+      input.value = item.cert_id;
+      suggestionList.classList.add("hide");
+    });
+
+    suggestionList.appendChild(suggestionItem);
+  });
+}
+
 const btn = document.getElementById("btn");
 
 btn.addEventListener("click", (e) => {
   e.preventDefault();
   const certificateCode = input.value;
-  console.log(certificateCode)
   msg.innerText = "";
-  data.map((item) => {
+  data.forEach((item) => {
     if (item.cert_id === certificateCode) {
-      console.log("hello")
       result.innerHTML = `
         <table>
           <tr>
