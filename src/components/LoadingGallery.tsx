d14e22aab34df70fbad5381915d224695e03e221
Abhishek Niraulanpx install react-spinners <080bei004@ioepc.edu.np>
2024-11-13 22:07:34 +0545

    Gallery Final

---
+++ src/components/LoadingGallery.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '@/src/app/loadingGallery.css';
+
+const Loading = () => {
+	return (
+		<>
+			<span className="loader"></span>
+		</>
+	);
+};
+export default Loading;
