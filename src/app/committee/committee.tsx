1bf387ff2d00c49ec29349da1a6860cffd058ef7
AbhishekNiraula <080bei004@ioepc.edu.np>
2025-04-22 22:38:31 +0545

    Changes

--- src/app/committee/committee.tsx
+++ src/app/committee/committee.tsx
@@ -35,7 +35,7 @@ const MemberCard = ({
 			{member.photo ? (
 				<div className="mb-3">
 					<Image
-						src={`./images/${CommitteeList[activeIndex].year}/${member.photo}`}
+						src={`/images/${CommitteeList[activeIndex].year}/${member.photo}`}
 						alt={member.name}
 						width={128}
 						height={128}
